b393e8981860f6d84a7d6a7e6cdef200b568a878
AVINASH-P-1912 <102513343+AVINASH-P-1912@users.noreply.github.com>
2022-04-22 14:04:51 +0530

    Add files via upload

---
+++ 3_Implementation/src/main.c
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+#include <util/delay.h>
+int main(void)
+{
+	DDRB=0xFF;
+	unsigned char z;
+	while(1)
+	{
+		
+		for(z=0;z<255;z++)
+		{
+			PORTB=z;
+			_delay_ms(1000);
+		}
+	}
+	return 0;
+}
